20e61bc48dc45761ecffb2936ffc19ae5a81e916
chalosalvador <chalosalvador@gmail.com>
2021-11-09 20:44:34 -0500

    #2 hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,22 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      sm: "600px",
+      md: "900px",
+      lg: "1200px",
+      xl: "1536px",
+    },
     colors: {
       transparent: "transparent",
       current: "currentColor",
+      pink: "#ff75b7",
+      red: "#e84f5a",
+      green: "#54ffe0",
       black: colors.black,
       white: colors.white,
       gray: colors.trueGray,
-      green: "#54FFE0",
       indigo: colors.indigo,
-      red: colors.rose,
       yellow: colors.amber,
     },
     fontFamily: {
@@ -24,7 +31,18 @@ module.exports = {
       inter: ["Inter"],
     },
     fontSize: {
-      tiny: "0.5rem",
+      tiny: ".5rem",
+      xs: ".75rem",
+      sm: ".8125rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+      "7xl": "5rem",
     },
     gradientColorStops: () => ({
       primary: "#e14551",
@@ -33,6 +51,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         main: "linear-gradient(305.47deg, #E14551 46.87%, #FF75B7 129.51%);",
+        "hero-image": "url('/images/bg-hero.png')",
       },
     },
   },
