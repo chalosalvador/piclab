c0fb3e564e2d586e8a8ca3764199cbbb52de47b2
chalosalvador <chalosalvador@gmail.com>
2021-11-11 07:31:12 -0500

    "#10 Hero logo"

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,20 @@
 import Image from "next/image";
-import bgHero from "../../public/images/bg-hero.png";
+import bgHero from "/public/images/bg-hero.png";
+import logo from "/public/images/logo-piclab.png";
 
 const Hero = () => {
   return (
     <div className="flex h-hero relative">
-      <div className="padded-content-l w-full sm:w-2/4 md:w-1/3 absolute sm:relative z-10 bg-red bg-opacity-60 sm:bg-transparent flex flex-col justify-evenly h-full">
+      <div className="padded-content-l pt-8 pb-28 w-full sm:w-2/4 md:w-1/3 absolute sm:relative z-10 flex flex-col justify-between h-full">
+        <div className="sticky top-5">
+          <Image src={logo} width={118} height={39.76} alt="PICLAB" />
+        </div>
         <div>
           <span className="px-2 py-1 font-medium rounded-md border font-idlewild text-tiny text-green border-green">
             Coming summer 2021
           </span>
 
-          <div className="text-white text-lg sm:text-xl mt-4">
+          <div className="text-white text-xl mt-4">
             Powerful image <br />{" "}
             <span className="font-semibold">asset management</span>
             <br /> for everyone.
@@ -26,7 +30,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="w-full sm:w-2/4 md:2/3 relative opacity-30 sm:opacity-100">
+      <div className="w-full sm:w-2/4 md:2/3 relative opacity-10 sm:opacity-100">
         <div className="absolute overflow-y-hidden h-full">
           <Image
             src={bgHero}
--- src/components/RequestBetaInput.js
+++ src/components/RequestBetaInput.js
@@ -7,9 +7,11 @@ const RequestBetaInput = () => {
       <input
         type="email"
         placeholder="Enter email"
-        className="text-sm focus-visible:outline-none placeholder-gray-500 h-full sm:w-52"
+        className="text-sm focus-visible:outline-none placeholder-gray-500 h-full lg:w-52"
       />
-      <button className="button border-l border-gray-400">Request Beta</button>
+      <button className="button border-l border-gray-400 w-32">
+        Request Beta
+      </button>
     </div>
   );
 };
