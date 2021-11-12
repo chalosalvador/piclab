3947b06b13554469b30ca6756da2a7d77c60ca30
chalosalvador <chalosalvador@gmail.com>
2021-11-12 02:12:12 -0500

    Optimize css loading

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,27 +11,11 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            rel="preload"
+            rel="stylesheet"
             href="https://cloud.typography.com/6929016/7418832/css/fonts.css"
-            as="style"
-            onLoad="this.onload=null;this.rel='stylesheet'"
           />
-          <noscript>
-            <link
-              rel="stylesheet"
-              href="https://cloud.typography.com/6929016/7418832/css/fonts.css"
-            />
-          </noscript>
 
-          <link
-            rel="preload"
-            href="https://rsms.me/inter/inter.css"
-            as="style"
-            onLoad="this.onload=null;this.rel='stylesheet'"
-          />
-          <noscript>
-            <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-          </noscript>
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
 
           <script
             src="https://kit.fontawesome.com/117bcfe378.js"
