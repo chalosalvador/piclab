2946b4768cb169d781f56164ad711d8e5834780a
chalosalvador <chalosalvador@gmail.com>
2021-11-12 12:33:35 -0500

    restore fonts and parallax

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,20 +1,21 @@
 import PropTypes from "prop-types";
+import { animated } from "react-spring";
 
-const Tag = ({ text, className }) => {
+const Tag = ({ text, className, transform }) => {
   return (
-    <div
+    <animated.div
       className={`will-change-transform text-white border-white border rounded-full px-3 backdrop-filter backdrop-blur absolute before:content-['#'] before:mr-2 before:opacity-50 ${className}`}
-      // style={{ transform }}
+      style={{ transform }}
     >
       {text}
-    </div>
+    </animated.div>
   );
 };
 
 Tag.propTypes = {
   text: PropTypes.string,
   className: PropTypes.string,
-  // transform: PropTypes.any,
+  transform: PropTypes.any,
 };
 
 export default Tag;
