646d849a08f6f95deba525904d912cd3f2f3171e
chalosalvador <chalosalvador@gmail.com>
2021-11-12 12:09:00 -0500

    remove fonts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,13 +10,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link
-            rel="stylesheet"
-            href="https://cloud.typography.com/6929016/7418832/css/fonts.css"
-          />
-
-          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-
           <script
             src="https://kit.fontawesome.com/117bcfe378.js"
             crossOrigin="anonymous"
